fdb1c90a3bf7198c6ba2e0319f3210d419546a0d
famevex <famevex.dev@gmail.com>
2026-04-24 12:29:46 +0300

    feat: bootstrap telegram bot with polling

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/famevex/eth-wallet-watcher
 
 go 1.26.1
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	gopkg.in/telebot.v3 v3.3.8 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	telebot "gopkg.in/telebot.v3"
+)
 
 func main() {
-	fmt.Println("eth-wallet-watcher starting...")
+	godotenv.Load()
+	telegram_token := os.Getenv("TELEGRAM_TOKEN")
+	proxy_url := os.Getenv("PROXY_URL")
+	
+	proxyURL, err := url.Parse(proxy_url) // bring link to the type *url.URL (for Client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// сreate a client that works through a proxy
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+	}
+
+	// set up the bot settings
+	pref := telebot.Settings{
+		Token: telegram_token,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		Client: client,
+	}
+
+	b, err := telebot.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// /start
+	b.Handle("/start", func(c telebot.Context) error {
+		return c.Send("Hi, I'm eth-wallet-watcher-bot")
+	})
+
+	// plain text
+	b.Handle(telebot.OnText, func(c telebot.Context) error {
+		return c.Send("You wrote: " + c.Text())
+	})
+
+	log.Println("Bot started...")
+	b.Start()
 }
